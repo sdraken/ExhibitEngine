3c7ffdece181a9c0c54494fdcd483000faacad66
Svante Drakenberg <sdraken@gmail.com>
2024-12-27 17:33:03 +0100

    File and dependency structure.
    Using pitchfork project layout. Submodules are used to clearly show dependencies of project.

---
+++ src/test.cpp
@@ -0,0 +1,11 @@
+#include <vk_mem_alloc.h>
+#include <glm/vec3.hpp>
+#include <stb_image.h>
+#include <tiny_obj_loader.h>
+
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello ExhibitEngine!" << std::endl;
+    return 0;
+}
